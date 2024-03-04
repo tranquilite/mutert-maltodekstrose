08432787d427678b8a070a4d5536f7a042367b43
Mathias <mathias.moe@gmail.com>
2024-03-04 20:25:51 +0100

    Revert "Nullstiller for v4"
    This reverts commit c99ad4992cb2d9ef51a00a3c13a4ecf4bb124f5b.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import express, { Express, NextFunction, Request, Response } from "express";
-import {Spilltilstand} from './models';
+import {Spilltilstand, Spiller, Kort, Bud} from './models';
 const path = require("path");
 
 const app: Express = express();  // http://expressjs.com/en/5x/api.html#app.use
@@ -16,17 +16,61 @@ app.use(function(inRequest: Request, inResponse: Response, inNext: NextFunction)
 
 // Bygger ut litt feilhåndtering
 function Trainwreck(err: any, tree: string='NA') { console.log(err); }
-function kontroller_klartilstand(system: Spilltilstand) {
-    return system.system_klar();
+
+
+/* =========================================================================
+    Men jeg må få en oblig som er server-basert, som holder rede på 4 spillere,
+    gir dem 13 kort hver og lar dem telle opp poengene.
+    Om de teller dem riktig eller feil spiller ingen rolle -
+    det er ikke det essensielle her.
+
+    Forsøk å få til at den kan dele kort, telle poeng og la spillerne
+    melde én melding og få ett svar (fra sin "makker")
+   ========================================================================= */ 
+function generer_kortstokk() {
+    let kortstokk: Kort[] = [];
+    let farger: string[] = ["Spar", "Hjerter", "Ruter", "Kløver"];
+    let valor: string[] = ["2", "3", "4", "5", "6", "7", "8", "9",
+                           "10", "J","D", "K", "A"]
+
+    for (let farge of farger) 
+    {  // Hvis lat, overkompliser.
+        for (let verdi of valor) 
+        {
+            kortstokk.push(new Kort(farge, verdi));
+        }
+    }
+
+    // Stokk om. ..tror jeg. tbh husker jeg dette knapt
+    for (let i = kortstokk.length - 1; i > 0; i--)
+    {
+        const base: number = Math.floor(Math.random() * (i + 1));
+        [kortstokk[i], kortstokk[base]] = [kortstokk[base], kortstokk[i]];
+    }
+
+    return kortstokk;
 }
 
-// Grensesnitt
+
+// Start et spill.
+// Ta et array med fire navn og returner et array Spiller-objekt
 app.post("/bridge/start",
     (req: Request, resp: Response) => {
+        let spillere: Spiller[] = [];
+        const kortstokk = generer_kortstokk();
+
         try
         {
-            GSYSTEM.registrer()
-            resp.send(kontroller_klartilstand(GSYSTEM))
+            const spillforesporsel = req.body["spillere"];
+            for (let i = 0; i < 4; i++)
+            {
+                const hand = kortstokk.slice( (13*i), (13*(i+1)) );
+                spillere.push(new Spiller(spillforesporsel[i],
+                                          i*4, // "TODO: ag skikkelige id-er"
+                                          hand, i));
+            }
+            SPILL.set_game_state(spillere, {});
+            resp.send(spillere);
         }
         catch (inError)
         {
@@ -34,11 +78,22 @@ app.post("/bridge/start",
         }
 });
 
-app.post("/bridge/registrer",
+app.post("/bridge/bid/:player_id",
     (req: Request, resp: Response) => {
         try
         {
-
+            if (parseInt(req.params.player_id) == SPILL.get_budgiver())
+            {
+                let _bud: Bud = {
+                    rank: parseInt(req.body["bid_rank"]),
+                    bud: parseInt(req.body["bid_suit"])
+                }
+                SPILL._gi_bud(_bud);
+            }
+            else
+            {
+                resp.send( {"error": "Wrong turn"} );
+            }
         }
         catch (inError)
         {
@@ -46,12 +101,18 @@ app.post("/bridge/registrer",
         }
 });
 
+app.get("/bridge/bid",
+    (req: Request, resp: Response) => {
+        
+        resp.send( SPILL.get_bud() );
+});
 
-// Håndter /
+
+// Håndter rooooot
 app.get("/", async(req: Request, resp: Response) => {
     try
     {
-        resp.send(kontroller_klartilstand);
+        resp.sendFile(path.join(__dirname, 'frontend.htm'));
     }
     catch (inError)
     {
@@ -59,6 +120,6 @@ app.get("/", async(req: Request, resp: Response) => {
     }
 });
 
-// ..
-var GSYSTEM = new Spilltilstand();  // NEI! Slem!
+// Fyr løs
+var SPILL = new Spilltilstand();  // NEI! Slem! Global var er ondskap
 app.listen(80, () => console.log("Kjører..") );
